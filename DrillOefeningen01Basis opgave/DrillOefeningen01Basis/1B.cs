8974c22358c2cc825faa1c58b85a938368cd0653
Ineslucia <147160819+Ineslucia@users.noreply.github.com>
2025-10-01 14:32:53 +0200

    Add files via upload

---
+++ DrillOefeningen01Basis opgave/DrillOefeningen01Basis/1B.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Voer je naam in: ");
+        string naam = Console.ReadLine();
+        Console.WriteLine($"Hallo, {naam}!");
+
+    }
---
+++ DrillOefeningen01Basis opgave/DrillOefeningen01Basis/3A.cs
@@ -0,0 +1,16 @@
+﻿namespace A3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"------------
+                                            
+
+                                   ---       
+                                  (O O)
+                                (   V  )
+                              -----m-m----      ");
+        }
+    }
+}
