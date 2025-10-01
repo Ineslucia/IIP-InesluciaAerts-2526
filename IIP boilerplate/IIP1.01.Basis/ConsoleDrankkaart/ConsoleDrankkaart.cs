47bd2a986412317c225ba2b15ea51b7cf601e9c1
Ineslucia <147160819+Ineslucia@users.noreply.github.com>
2025-10-01 15:33:52 +0200

    Add files via upload

---
+++ IIP boilerplate/IIP1.01.Basis/ConsoleDrankkaart/ConsoleDrankkaart.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleDrankkaart
+{
+    class Program
+    {
+        static void Main(string[] args)
+		
+        {
+			
+            Console.WriteLine("@====== MENU ======");
+            Console.WriteLine("☕ Koffie:   3€");
+            Console.WriteLine("🍵 Thee:    2€");
+            Console.WriteLine("🥤 Limo:    2€");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("copyright ©2025\n");
+
+            string pad = @"C:\Dropbox\MijnCafe\menu.pdf";
+            Console.WriteLine($"Het menu staat ook op \"{pad}\"");
+        }
+    }
+}
