62a94541b3635861c4c44f4cfb62f42f040643e8
Ineslucia <147160819+Ineslucia@users.noreply.github.com>
2025-10-01 15:47:55 +0200

    Add files via upload

---
+++ DrillOefeningen01Basis opgave/DrillOefeningen01Basis/2B/2B.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welkom in de echoput");
+        Console.Write("Zeg iets: ");
+        string echo = Console.ReadLine();
+        Console.Write($"{echo}! {echo}! {echo}!");
+    }
+}
