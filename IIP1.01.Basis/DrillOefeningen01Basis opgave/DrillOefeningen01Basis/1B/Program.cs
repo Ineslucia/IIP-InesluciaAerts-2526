e00b4f364c95be79577976163615ca5fa6793818
Ineslucia <147160819+Ineslucia@users.noreply.github.com>
2025-10-08 15:51:38 +0200

    Add files via upload

---
+++ IIP1.01.Basis/DrillOefeningen01Basis opgave/DrillOefeningen01Basis/1B/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Voer je naam in: ");
+        string naam = Console.ReadLine();
+
+        Console.WriteLine($"Hallo, {naam}!");
+
+    }
+}
