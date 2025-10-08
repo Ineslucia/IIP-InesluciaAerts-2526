d2cb228547865269741681a3d4e4203fdb243a90
Ineslucia <147160819+Ineslucia@users.noreply.github.com>
2025-10-08 15:49:56 +0200

    Add files via upload

---
+++ IIP1.01.Basis/DrillOefeningen01Basis opgave/DrillOefeningen01Basis/3A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+ namespace A3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"------------
+                                            
+
+                                   ---       
+                                  (O O)
+                                (   V  )
+                              -----m-m----      ");
+            Console.ReadKey();
+        }
+    }
+}
